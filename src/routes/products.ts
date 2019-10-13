e8b9de0621c7031a95093f973138abf9693a7e2e
buycs <buyco.michael@gmail.com>
2019-10-13 19:04:06 +0800

    Initial template

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -5,5 +5,9 @@ import ProductsController from "../controllers/products";
 const router = Router();
 
 router.get("/", ProductsController.list);
+router.get("/:id", ProductsController.get);
+router.post("/", ProductsController.create);
+router.put("/:id", ProductsController.update);
+router.delete("/:id", ProductsController.destroy);
 
 export default router;
